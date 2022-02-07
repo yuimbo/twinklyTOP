1a71a99434962fcfe3ece7c7007c4b65b3c86973
Jimmy Ghaderi <jimmy.ghaderi@svt.se>
2022-02-07 10:13:32 +0100

    sketching, threaded send

--- td_operator.py
+++ td_operator.py
@@ -2,13 +2,13 @@
 # scriptOp - the OP which is cooking
 import sys
 #https://docs.derivative.ca/Category:Python#Installing_Custom_Python_Packages
-mypath = "/Users/jimmyghaderi/.pyenv/versions/3.7.12/lib/python3.7/site-packages"
-if mypath not in sys.path:
-    sys.path.append(mypath)
-
+modules_path = "/Users/jimmyghaderi/.pyenv/versions/3.7.12/lib/python3.7/site-packages"
+if modules_path not in sys.path:
+    sys.path.append(modules_path)
 
 import xled
 import numpy
+import threading
 import io
 
 def deviceToController(device):
@@ -18,24 +18,21 @@ def deviceToController(device):
 def deviceNames(device):
     return device.id
 
-devices = []
+def discoverDevices():
+    devices = []
+    try:
+        for device in xled.discover.xdiscover(timeout=2):
+            devices.append(device)
+    except xled.exceptions.DiscoverTimeout as e:
+        pass
+    return sorted(devices, key=lambda d: d.id)
 
-try:
-    for device in xled.discover.xdiscover(timeout=2):
-        devices.append(device)
-except xled.exceptions.DiscoverTimeout as e:
-    pass
-
-
-devices = sorted(devices, key = lambda d: d.id)
-
+devices = discoverDevices()
 controls = list(map(lambda d: deviceToController(d), devices))
 for control in controls:
     control.set_mode("rt")
 layouts = list(map(lambda c: c.get_led_layout().data, controls))
 device_infos = list(map(lambda c: c.get_device_info().data, controls))
-
-
 
 def flatten(inlist):
     return [a for tup in inlist for a in tup]
@@ -44,50 +41,51 @@ def normalizedFloatToInt8(float_val):
     return round(float_val * 255)
 
 def renormalize(coord):
-    return (coord +1)/2
+    return (coord + 1) / 2
 
-def makeImg(input_op, coords, rgbw=False):
+def remapImg(image, coords, rgbw=False):
     out = []
+    [h, w] = image.shape
     for coord in coords:
-        colours = input_op.sample(
-            u=renormalize(coord['x']), v=coord['y'])
-        
-        [r, g, b, a] = list(map(lambda c: normalizedFloatToInt8(c), colours))
+        channels = image[
+            round(renormalize(coord['y'] * h)), 
+            round(renormalize(coord['x'] * w))]
+        [r, g, b, a] = list(map(lambda c: normalizedFloatToInt8(c), channels))
         if rgbw:
-            out.append([r, g, b, 0])
+            channels = [0, r, g, b]
         else:
-            out.append([r,g,b])
-
+            channels = [r,g,b]
+        out.append([c * 255 for c in channels])
     return flatten(out)
 
-device_par = None
-previous_device_par = None
+
+def sendImage(control, coords, image, rgbw=False):
+    frame = io.BytesIO(bytes(
+        remapImg(image, coords, rgbw)))
+    #TODO: Handle closing previous socket when changing device
+    control.set_rt_frame_socket(frame, 3)
 
 def onSetupParameters(scriptOp):
     page = scriptOp.appendCustomPage('Twinkly')
     [device_par] = page.appendMenu("Device")
-
     device_par.menuNames = list(range(len(devices)))
-    device_par.menuLabels = list(map(lambda d: deviceNames(d), devices))
-
+    device_par.menuLabels = [d.id for d in devices]
     return
 
-
-def onPulse(par):
-    return
-
-
+t = threading.Thread()
 def onCook(scriptOp):
     current_id = scriptOp.pars("Device")[0]
     control = controls[current_id]
-
-    layout = layouts[current_id]
-    coords = layout['coordinates']
-
-    frame = io.BytesIO(bytes(
-        makeImg(scriptOp.inputs[0], coords, device_infos[current_id]['led_profile'] == 'RGBW')))
-
-    #TODO: Handle closing previous socket when changing device
-    control.set_rt_frame_socket(frame, 3)
-
+    coords = layouts[current_id]['coordinates']
+    if not t.is_alive():
+        t = threading.Thread(
+            target=sendImage, 
+            daemon=True,
+            args=(
+                control,
+                coords,
+                scriptOp.inputs[0].numpyArray(),
+                device_infos[current_id]['led_profile'] == 'RGBW'),
+            )
+        t.start()
     return
